8f88516d2b638cf366e110def06c2d1235ee0efd
Mateusz Wszola <matwsz000@utp.edu.pl>
2022-06-27 23:03:44 +0200

    feat: Implemented Zadanie service

--- src/main/java/com/project/service/ZadanieServiceImpl.java
+++ src/main/java/com/project/service/ZadanieServiceImpl.java
@@ -1,44 +1,104 @@
 package com.project.service;
 
-import com.project.model.Student;
+import com.project.model.Zadanie;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.Optional;
 
-public class ZadanieServiceImpl implements StudentService {
-    @Override
-    public Optional<Student> getStudent(Integer studentId) {
-        return Optional.empty();
+@SuppressWarnings("SpringJavaAutowiredMembersInspection")
+public class ZadanieServiceImpl implements ZadanieService {
+    private static final Logger logger = LoggerFactory.getLogger(ZadanieServiceImpl.class);
+
+    @Value("${rest.server.url}")
+    private String serverUrl;
+
+    private final static String RESOURCE_PATH = "/api/zadania";
+
+    private RestTemplate restTemplate;
+
+    @Autowired
+    public ZadanieServiceImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
     @Override
-    public Student setStudent(Student student) {
-        return null;
+    public Optional<Zadanie> getZadanie(Integer zadanieId) {
+        URI url = ServiceUtil.getUriComponent(serverUrl, getResourcePath(zadanieId))
+                .build()
+                .toUri();
+        logger.info("REQUEST -> GET {}", url);
+        return Optional.ofNullable(restTemplate.getForObject(url, Zadanie.class));
     }
 
     @Override
-    public void deleteStudent(Integer studentId) {
-
+    public Zadanie setZadanie(Zadanie zadanie) {
+        if (zadanie.getZadanieId() != null) {
+            String url = getUriStringComponent(zadanie.getZadanieId());
+            logger.info("REQUEST -> PUT {}", url);
+            restTemplate.put(url, zadanie);
+            return zadanie;
+        } else {
+            HttpEntity<Zadanie> request = new HttpEntity<>(zadanie);
+            String url = getUriStringComponent();
+            logger.info("REQUEST -> POST {}", url);
+            URI location = restTemplate.postForLocation(url, request);
+            logger.info("REQUEST (location) -> GET {}", location);
+            return restTemplate.getForObject(location, Zadanie.class);
+        }
     }
 
     @Override
-    public Page<Student> getStudenci(Pageable pageable) {
-        return null;
+    public void deleteZadanie(Integer zadanieId) {
+        URI url = ServiceUtil.getUriComponent(serverUrl, getResourcePath(zadanieId))
+                .build()
+                .toUri();
+        logger.info("REQUEST -> DELETE {}", url);
+        restTemplate.delete(url);
     }
 
     @Override
-    public Optional<Student> getStudentByNrIndeksu(String nrIndeksu) {
-        return Optional.empty();
+    public Page<Zadanie> getZadania(Pageable pageable) {
+        URI url = ServiceUtil.getURI(serverUrl, getResourcePath(), pageable);
+        logger.info("REQUEST -> GET {}", url);
+        return getPage(url, restTemplate);
     }
 
     @Override
-    public Page<Student> getStudenciByNrIndeksuStartsWith(String nrIndeksu, Pageable pageable) {
-        return null;
+    public Page<Zadanie> getZadaniaProjektu(Integer projektId, Pageable pageable) {
+        URI url = ServiceUtil.getUriComponent(serverUrl, getResourcePath(), pageable)
+                .queryParam("projektId", projektId)
+                .build().toUri();
+        logger.info("REQUEST -> GET {}", url);
+        return getPage(url, restTemplate);
     }
 
-    @Override
-    public Page<Student> getStudenciByNazwiskoStartsWith(String nazwisko, Pageable pageable) {
-        return null;
+    private Page<Zadanie> getPage(URI uri, RestTemplate restTemplate) {
+        return ServiceUtil.getPage(uri, restTemplate, new ParameterizedTypeReference<RestResponsePage<Zadanie>>() {
+        });
+    }
+
+    private String getResourcePath() {
+        return RESOURCE_PATH;
+    }
+
+    private String getResourcePath(Integer id) {
+        return RESOURCE_PATH + "/" + id;
+    }
+
+    private String getUriStringComponent() {
+        return serverUrl + getResourcePath();
+    }
+
+    private String getUriStringComponent(Integer id) {
+        return serverUrl + getResourcePath(id);
     }
 }
